888823b44d71db6a13ea23287cf40416bfbd3da4
laura rodriguez <laura.rodriguez.king@outlook.com>
2023-09-30 17:21:40 -0500

    preparacion de ambiente

---
+++ src/users/user.entity.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+// Modelo encargado de crear las clases y base de datos de los usuarios
+@Entity()
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    full_name: string;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+
+    // @Column()
+    // phone:string;
+
+    // @Column()
+    // role:string;
+
+    // @Column()
+    // is_deleted:string;
+
+    // @Column()
+    // created_at:string;
+
+    // @Column()
+    // updated_at:string;
+}
+
+
