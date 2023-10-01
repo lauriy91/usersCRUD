f2ba5e8954218f5bd641d334f62fb8dae4b23e73
laura rodriguez <laura.rodriguez.king@outlook.com>
2023-10-01 13:51:16 -0500

    validaciones extras

--- src/roles/parameters/rol.dto.ts
+++ src/roles/parameters/rol.dto.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
+import { IsBoolean, IsString } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RolDto {
   @ApiProperty()
+  @IsString()
   readonly name: string;
 
   @ApiProperty()
+  @IsBoolean()
   readonly is_deleted: boolean;
 
   constructor(name: string, is_deleted: boolean) {
--- src/users/parameters/user.dto.ts
+++ src/users/parameters/user.dto.ts
@@ -1,24 +1,36 @@
 /* eslint-disable prettier/prettier */
+import { Transform } from '@nestjs/class-transformer';
+import { IsBoolean, IsEmail, IsPhoneNumber, IsString, MaxLength, MinLength } from '@nestjs/class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UserDTO {
   @ApiProperty()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
   readonly full_name: string;
 
   @ApiProperty()
+  @IsEmail()
   readonly email: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(6)
+  @Transform(({ value }) => value.trim())
   readonly password: string;
 
   @ApiProperty()
   @ApiPropertyOptional()
+  @IsPhoneNumber()
+  @MinLength(10)
   readonly phone?: number;
 
   @ApiProperty()
   readonly role: string;
 
   @ApiProperty()
+  @IsBoolean()
   readonly is_deleted: boolean;
 
   constructor(full_name: string, email: string, password: string, phone: number, role: string, is_deleted: boolean) {
